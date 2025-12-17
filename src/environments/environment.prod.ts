754f51a312c5f70904a9ac072ecef28e6670e6ee
victor-ss13 <victor.sampaio2003@gmail.com>
2025-12-17 14:29:23 -0300

    feat: environments prontos

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.stockflow.com/api', // Substituir pela URL de produção
+  apiTimeout: 30000, // 30 segundos
+  enableDebugLogs: false
+};
